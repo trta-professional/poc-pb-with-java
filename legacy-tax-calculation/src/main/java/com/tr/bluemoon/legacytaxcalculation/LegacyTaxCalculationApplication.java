a6684d739828994841cadead47e6474c4a0ad2b3
Tiago Marques <tiago.marques@thomsonreuters.com>
2020-01-10 16:51:59 -0300

    Add legacy-tax-calculation project

---
+++ legacy-tax-calculation/src/main/java/com/tr/bluemoon/legacytaxcalculation/LegacyTaxCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.tr.bluemoon.legacytaxcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyTaxCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyTaxCalculationApplication.class, args);
+	}
+
+}
---
+++ legacy-tax-calculation/src/test/java/com/tr/bluemoon/legacytaxcalculation/LegacyTaxCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.bluemoon.legacytaxcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyTaxCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
